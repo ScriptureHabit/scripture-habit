37dafd1f9847f6f5a62ff301b3d0826e5b55afae
Daijir <dazhilangxianggen@gmail.com>
2026-07-06 20:28:34 +0900

    fix

--- scripture-habit/playwright.config.ts
+++ scripture-habit/playwright.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: 'cross-env VITE_USE_FIREBASE_EMULATOR=true npm run dev',
+      command: 'cross-env VITE_USE_FIREBASE_EMULATOR=true npm run dev -- --host 127.0.0.1',
       url: 'http://127.0.0.1:5173',
       reuseExistingServer: !process.env.CI,
       stdout: 'pipe',
